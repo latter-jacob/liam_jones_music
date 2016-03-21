9e9cbe4feed8ac708b3d415a5e3927024c1bb5ed
latter-jacob <latter.jacob@gmail.com>
2016-03-21 14:55:03 -0400

    about page routed with pic and sentence

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :songs, only: [:index, :new, :create, :destroy, :show]
   resources :videos, only: [:index, :new, :create, :destroy, :show]
   root 'homes#index'
+  get '/about', to: 'homes#about'
   devise_for :users
 end
