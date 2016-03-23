19748a5dd4a0b8226c7ebe49ae58220273d0ff8d
latter-jacob <latter.jacob@gmail.com>
2016-03-23 14:50:20 -0400

    downloading for songs works

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-
-  resources :songs, only: [:index, :new, :create, :destroy, :show]
-  resources :videos, only: [:index, :new, :create, :destroy, :show]
   root 'homes#index'
+
+  resources :songs, only: [:index, :new, :create, :destroy, :show] do
+      get 'download' => "songs#download", as: "download"
+  end
+
+  resources :videos, only: [:index, :new, :create, :destroy, :show]
+
   get '/about', to: 'homes#about'
+
   devise_for :users
+
 end
